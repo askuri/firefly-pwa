2941e5ee271cbb18fcf1e6d04961f211be7b3bea
Martin Weber <enwi2@t-online.de>
2021-01-29 18:07:59 +0100

    submitting of transactions works now

--- src/app/pages/transaction-add/transaction-add.component.ts
+++ src/app/pages/transaction-add/transaction-add.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import {ApiClientService} from '../../services/api-client.service';
+import {operations} from '../../api-interface/firefly';
 
 @Component({
   selector: 'app-transaction-add',
@@ -20,13 +22,39 @@ export class TransactionAddComponent implements OnInit {
     notes: new FormControl('')
   });
 
-  constructor() { }
+  constructor(
+    private apiClientService: ApiClientService
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
     // TODO: Use EventEmitter with form value
-    console.warn(this.addTransactionForm.value);
+    const formValues = this.addTransactionForm.value;
+
+    const request: operations['storeTransaction']['requestBody']['application/json'] = {
+      error_if_duplicate_hash: false,
+      apply_rules: false,
+      transactions: [
+        {
+          type: 'withdrawal', // TODO we should find out ourselves what is correct
+          description: formValues.description,
+          source_id: formValues.sourceAccount.id,
+          destination_id: formValues.destinationAccount.id,
+          date: formValues.date.toISOString(),
+          amount: formValues.amount,
+          budget_id: formValues.budget.id,
+          category_id: formValues.category.id,
+          bill_id: formValues.bill.id,
+          notes: formValues.notes
+        }
+      ]
+    };
+
+    this.apiClientService.storeTransaction(request).subscribe(data => {
+      console.warn('Submitted new Transaction, received: ', data);
+    });
   }
 }
--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ApiTokenService} from './api-token.service';
 import {Observable} from 'rxjs';
+import {components, operations} from '../api-interface/firefly';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,19 @@ export class ApiClientService {
         Authorization: 'Bearer ' + this.apiTokenService.getToken()
       }
     });
+  }
+
+  public post(url: string, body): Observable<object> {
+    return this.http.post(url, body, {
+      headers: {
+        Authorization: 'Bearer ' + this.apiTokenService.getToken()
+      }
+    });
+  }
+
+  public storeTransaction(transaction: operations['storeTransaction']['requestBody']['application/json']):
+    Observable<components['schemas']['TransactionSingle'] | components['schemas']['ValidationError']>
+  {
+    return this.post('api/v1/transactions', transaction) as Observable<components['schemas']['TransactionSingle'] | components['schemas']['ValidationError']>;
   }
 }
